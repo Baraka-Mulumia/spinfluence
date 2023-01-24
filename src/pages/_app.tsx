b169b9a1790d71833846a98375926deb41c3fc16
Baraka M Mulumia <65751779+barakamulumia@users.noreply.github.com>
2023-01-24 20:14:40 +0300

    feat: Add Custom head component for SEO

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,12 @@ import '@/styles/globals.css';
 
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
+import SPHead from '@/components/SPHead';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
+      <SPHead />
       <Component {...pageProps} />
     </ChakraProvider>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import { Box, Text, VStack } from '@chakra-ui/react';
 
-import Head from 'next/head';
 import { Inter } from '@next/font/google';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -8,9 +7,6 @@ const inter = Inter({ subsets: ['latin'] });
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>SpinFluence</title>
-      </Head>
       <main>
         <Box>
           <VStack
