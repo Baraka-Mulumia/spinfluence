ae7a84fad7d2b341338f79bbbaa5522849af8106
Baraka Mshindi Mulumia <65751779+iambaraka@users.noreply.github.com>
2023-02-02 13:49:58 +0300

    Create MenuIcon.tsx

---
+++ src/assets/icons/MenuIcon.tsx
@@ -0,0 +1,10 @@
+import { EIcon } from '@/components/atoms/EIcon';
+import { FunctionComponent } from 'react';
+
+export const MenuIcon: FunctionComponent = () => {
+  return (
+    <EIcon>
+      <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" />
+    </EIcon>
+  );
+};
