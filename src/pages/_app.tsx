89467ecce25411806443ed60608d21337e19833f
Baraka Mshindi Mulumia <65751779+iambaraka@users.noreply.github.com>
2023-02-02 12:38:24 +0300

    configure theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,11 @@ import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import { SpinFluenceHead } from '@/components/molecules/SpinFluenceHead';
+import theme from '@/styles/theme';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <SpinFluenceHead />
       <Component {...pageProps} />
     </ChakraProvider>
