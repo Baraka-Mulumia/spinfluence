6593bfaa975a0822faa8926e8281f374c31ed769
Baraka Mshindi Mulumia <65751779+iambaraka@users.noreply.github.com>
2023-02-03 11:07:05 +0300

    feat: :sparkles: Add banner component
    closes #4

--- src/components/atoms/CtaButton.tsx
+++ src/components/atoms/CtaButton.tsx
@@ -1,23 +1,26 @@
-import { FunctionComponent } from 'react';
-import { Box, Button } from '@chakra-ui/react';
+import { Box, Button, ButtonProps } from '@chakra-ui/react';
 
+import { FunctionComponent } from 'react';
 import Link from 'next/link';
 
-export const CtaButton: FunctionComponent = () => {
+export const CtaButton: FunctionComponent<ButtonProps> = (props) => {
   return (
     <Box>
       <Link href={'/ignite'}>
         <Button
           variant="outline"
-          colorScheme="blue"
           fontSize={'sm'}
-          size={'sm'}
+          bg={'blue.500'}
+          color={'white'}
+          size={'md'}
+          borderRadius={'4px'}
           _hover={{
             bg: 'blue.500',
             color: 'white',
             transition: 'all 0.3s ease-in-out',
-            boxShadow: '0 0 0 2px #3182ce',
-          }}>
+            boxShadow: '0 0 0 1px #3182ce',
+          }}
+          {...props}>
           Get Started
         </Button>
       </Link>
---
+++ src/components/layout/SectionContainer.tsx
@@ -0,0 +1,23 @@
+import { Box, BoxProps, Container, Stack } from '@chakra-ui/react';
+import { FunctionComponent, PropsWithChildren } from 'react';
+
+export const SectionContainer: FunctionComponent<
+  PropsWithChildren<BoxProps>
+> = ({ children, ...props }) => {
+  return (
+    <Box
+      as={'section'}
+      w={'full'}
+      minH={'80vh'}
+      display={'flex'}
+      alignItems={'center'}
+      justifyContent={'center'}
+      {...props}>
+      <Container maxW={'container.xl'} h={'full'}>
+        <Stack spacing={8} py={8} h={'full'}>
+          {children}
+        </Stack>
+      </Container>
+    </Box>
+  );
+};
