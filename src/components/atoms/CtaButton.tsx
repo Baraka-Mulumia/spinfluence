2d82523b4a9cf00434602db5074dc75b58ab74b3
Baraka Mshindi Mulumia <65751779+iambaraka@users.noreply.github.com>
2023-02-04 19:54:03 +0300

    Update CtaButton.tsx

--- src/components/atoms/CtaButton.tsx
+++ src/components/atoms/CtaButton.tsx
@@ -3,14 +3,18 @@ import { Box, Button, ButtonProps } from '@chakra-ui/react';
 import { FunctionComponent } from 'react';
 import Link from 'next/link';
 
-export const CtaButton: FunctionComponent<ButtonProps> = (props) => {
+export const CtaButton: FunctionComponent<
+  ButtonProps & {
+    text?: string;
+  }
+> = ({ text = 'Get Started', ...props }) => {
   return (
     <Box>
       <Link href={'/'}>
         <Button
           variant="outline"
           fontSize={'sm'}
-          bg={'blue.500'}
+          bg={'blue.900'}
           color={'white'}
           size={'md'}
           borderRadius={'4px'}
@@ -18,10 +22,9 @@ export const CtaButton: FunctionComponent<ButtonProps> = (props) => {
             bg: 'blue.500',
             color: 'white',
             transition: 'all 0.3s ease-in-out',
-            boxShadow: '0 0 0 1px #3182ce',
           }}
           {...props}>
-          Get Started
+          {text}
         </Button>
       </Link>
     </Box>
