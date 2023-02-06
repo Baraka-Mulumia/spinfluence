cf887d369aa6e5bb132781b083295aa8deb0d516
Baraka Mshindi Mulumia <65751779+iambaraka@users.noreply.github.com>
2023-02-06 11:33:04 +0300

    Create EButton.tsx

---
+++ src/components/atoms/EButton.tsx
@@ -0,0 +1,30 @@
+import { Button, ButtonProps } from '@chakra-ui/react';
+
+import { FunctionComponent } from 'react';
+
+type EButtonProps = ButtonProps & {
+  text?: string;
+};
+
+export const EButton: FunctionComponent<EButtonProps> = ({
+  text = 'Get Started',
+}) => {
+  return (
+    <Button
+      variant="outline"
+      fontSize={'sm'}
+      bg={'blue.900'}
+      color={'white'}
+      size={'md'}
+      fontWeight={'bold'}
+      borderRadius={'4px'}
+      _hover={{
+        bg: 'blue.900',
+        color: 'white',
+        transition: 'all 0.3s ease-in-out',
+        boxShadow: '0 0 0 0.5px #3182ce',
+      }}>
+      {text}
+    </Button>
+  );
+};
